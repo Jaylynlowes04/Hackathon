5270f8e7941c81d30e64facf43419803fe591805
Jaylynlowes04 <117837026+Jaylynlowes04@users.noreply.github.com>
2023-11-10 10:56:56 -0500

    Add files via upload

---
+++ uber_pickups.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from langchain.llms import OpenAI
+llm = OpenAI(openai_api_key="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")
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+llm = OpenAI(openai_api_key="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")
+chat_model = ChatOpenAI(openai_api_key="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")
+
+
+st.title('Chat with TroyAI')
+
+question = st.text_input("Enter your Question","")
+if(question):
+    #Get answer from OpenAI
+    #st.write that answer to the user
+    #Make more personal
+    response = chat_model.predict(question)
+    st.write(response)
